26b103d9febdd3e1e394971688b10fc8a485e91c
github username <github email address>
2017-08-30 17:08:30 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,20 @@
 class Person
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
+
   def initialize(name)
     @name = name
     @bank_account = 25
     @happiness = 8
     @hygiene = 8
   end
+
   def call_friend(friend)
     self.happiness = self.happiness + 3
     friend.happiness = friend.happiness + 3
     return "Hi #{friend.name}! It's #{name}. How are you?"
   end
+
   def start_conversation(person, topic)
     if topic == "politics"
       self.happiness = self.happiness - 2
@@ -23,25 +28,12 @@ class Person
       return "blah blah blah blah blah"
     end
   end
-  def name
-    @name
-  end
-  def bank_account
-    @bank_account
-  end
-  def bank_account=(new_bank_account)
-    @bank_account = new_bank_account
-  end
+
   def get_paid(salary_increase)
     @bank_account = @bank_account + salary_increase
     return "all about the benjamins"
   end
-  def happiness
-    @happiness
-  end
-  def hygiene
-    @hygiene
-  end
+
   def happiness=(new_happiness)
     if new_happiness < 0
       @happiness = 0
@@ -51,6 +43,7 @@ class Person
       @happiness = new_happiness
     end
   end
+
   def happy?
     if @happiness > 7
       true
@@ -58,6 +51,7 @@ class Person
       false
     end
   end
+
   def hygiene=(new_hygiene)
     if new_hygiene < 0
       @hygiene = 0
@@ -67,6 +61,7 @@ class Person
       @hygiene = new_hygiene
     end
   end
+
   def clean?
     if @hygiene > 7
       true
@@ -74,10 +69,12 @@ class Person
       false
     end
   end
+
   def take_bath
     self.hygiene = self.hygiene + 4
     return "♪ Rub-a-dub just relaxing in the tub ♫"
   end
+
   def work_out
     self.hygiene = self.hygiene - 3
     self.happiness = self.happiness + 2
